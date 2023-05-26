81d887001ba76db2e1dedb8529c8cb367c15fb63
benjamin <benjaminvega7@gmail.com>
2023-05-26 16:48:33 -0700

    update

---
+++ Basic_HTML_and_JavaScript_1/Basic_HTML_1.html/Hello_World.js
@@ -0,0 +1,1 @@
+alert("Hello, World! Iam an alert box!");
---
+++ Basic_HTML_and_JavaScript_1/Basic_HTML_1.html/JavaScript 1.js
@@ -0,0 +1,8 @@
+<script>
+function My_First_Function () {
+var String = "Kiss me, i'm Irish!";
+var result = String.fontcolor("green");
+document.getElementById("Irish").innerHTML = 
+reslut;
+}
+</script>
